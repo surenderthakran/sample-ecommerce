95b45fe5447b44b392f61dd22965e412cf38fd7d
Yash Nehra <yash.nehra02@gmail.com>
2025-10-25 12:56:08 +0530

    add files

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Product Page</h1>
+    </div>
+  );
+};
+
+export default Product;
