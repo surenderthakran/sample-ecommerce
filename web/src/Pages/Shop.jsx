37a56226dadda901811e838a0a52466f47e80749
Yash nehra <84937726+yashnehra17@users.noreply.github.com>
2025-11-01 11:05:24 +0530

    16-code-is-structured-now (#17)
    * 16-code-is-structured-now
    
    * done
    
    * done 1

--- web/src/Pages/Shop.jsx
+++ web/src/Pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Hero from '../Components/Hero/Hero';
-import Popular from '../Components/Popular/Popular';
+import Hero from '.../Components/Hero/Hero';
+import Popular from '.../Components/Popular/Popular';
 
 const Shop = () => {
     return(
